c953dae505c6f917fa04af739e68d488368f9f9d
Rob <mail@robinweiss.de>
2015-01-22 18:42:30 +0100

    Preliminary version of left/right detection

--- First iteration/scripts/Synchronisation/synchronisation.m
+++ First iteration/scripts/Synchronisation/synchronisation.m
@@ -92,31 +92,94 @@ fs_GW = 200;
 % Set threshold for peak detection in acceleration signal and number of
 % samples between two cycles.
 threshold = 1.3;
-gap = 1200;
+gap = 1300;
 
 % Find all peaks greater than threshold.
-[peak_values, peak_locations] = findpeaks(a_Z_right_shank_1_C, ...
+[peak_values_l, peak_locations_l] = findpeaks(a_Z_left_shank_1_C, ...
                                 'minpeakheight', threshold);
-                         
+[peak_values_r, peak_locations_r] = findpeaks(a_Z_right_shank_1_C, ...
+                                'minpeakheight', threshold);
+
+                            
+%%
+
+% Plot for visualisation
+close all;
+plot(time, a_Z_right_shank_1_C);
+hold on;
+plot(time(peak_locations_r), a_Z_right_shank_1_C(peak_locations_r), 'r.');
+
+%%
+
+close all;
+plot(time, a_Z_left_shank_1_C, 'g');
+hold on;
+plot(time(peak_locations_l), a_Z_left_shank_1_C(peak_locations_l), 'k.');
+
+%%
+
 % Compute distance between two peaks.                             
-peak_distance = diff(peak_locations);
+peak_distance_l = diff(peak_locations_l);
+peak_distance_r = diff(peak_locations_r);
+
+%%
 
 % Create logical vector to select last peak of each cycle. That is the one
 % before a gap of at least 1200 samples, respectively. 
-select = peak_distance > gap;
+select_last_l = peak_distance_l > gap;
+select_last_r = peak_distance_r > gap;
+
+% close all;
+% plot(time, a_Z_left_shank_1_C, 'g');
+% hold on;
+% plot(time(peak_locations_l(select_l)), a_Z_left_shank_1_C(peak_locations_l(select_l)), 'k.');
+
+%%
 
 % Shift logical vector by one to the right to select the first instead of
 % the last peak of a cycle.
-first_peak = [0, select(1:length(select) - 1)'];
+select_first_l = [0, select_last_l(1:length(select_last_l) - 1)'];
+select_first_r = [0, select_last_r(1:length(select_last_r) - 1)'];
+
+%%
 
 % Add the very first detected peak.
-peaks = [peak_locations(1)', peak_locations(logical(first_peak))'];
+sync_peaks_l = [peak_locations_l(1)', peak_locations_l(logical(select_first_l))'];
+sync_peaks_r = [peak_locations_r(1)', peak_locations_r(logical(select_first_r))'];
 
-% Plot for verification.
+%%
+
+% Plot for visualisation
+close all;
 plot(time, a_Z_right_shank_1_C);
 hold on;
-plot(time(peaks), a_Z_right_shank_1_C(peaks), 'r.');
+plot(time(sync_peaks_r), a_Z_right_shank_1_C(sync_peaks_r), 'r.');
+
+%%
+
+close all;
+plot(time, a_Z_left_shank_1_C, 'g');
+hold on;
+plot(time(sync_peaks_l), a_Z_left_shank_1_C(sync_peaks_l), 'k.');
+
+%%
+clc;
+sync_peaks = [sync_peaks_r(sync_peaks_l > sync_peaks_r), sync_peaks_l(sync_peaks_r > sync_peaks_l)];
+
+sync_peaks = sort(sync_peaks);
+
+%%
+
+% Plot for verification.
+close all;
+plot(time, a_Z_left_shank_1_C);
+hold on;
+plot(time(sync_peaks_l(sync_peaks_r > sync_peaks_l)), a_Z_left_shank_1_C(sync_peaks_l(sync_peaks_r > sync_peaks_l)), 'r.');
+plot(time, a_Z_right_shank_1_C, 'g');
+plot(time(sync_peaks_r(sync_peaks_l > sync_peaks_r)), a_Z_right_shank_1_C(sync_peaks_r(sync_peaks_l > sync_peaks_r)), 'k.');
 hold off;
+
+%%
 
 % -------------------------------------------------------------------------
 % 4) Store GaitWatch signals and force plate signals in time series
@@ -127,26 +190,28 @@ hold off;
 a_trunk = createTimeseries([a_X_center_trunk_3_C; a_Y_center_trunk_3_C; ...
                             a_Z_center_trunk_3_C], time, ...
                             'Acceleration trunk', 'seconds', 'g', ...
-                            time(peaks), '. touch of force plate');
+                            time(sync_peaks), '. touch of force plate');
                         
 % Create time series of acceleration thigh.
 a_thigh = createTimeseries([a_X_left_thigh_1_C';  a_Z_left_thigh_1_C'; ...
                             a_X_right_thigh_1_C'; a_Z_right_thigh_1_C'], ...
                             time, 'Acceleration thigh', 'seconds', 'g', ...
-                            time(peaks), '. touch of force plate');
+                            time(sync_peaks), '. touch of force plate');
                         
 % Create time series of acceleration shank.
 a_shank = createTimeseries([a_X_left_shank_1_C';  a_Z_left_shank_1_C'; ...
                             a_X_right_shank_1_C'; a_Z_right_shank_1_C'], ...
                             time, 'Acceleration shank', 'seconds', 'g', ...
-                            time(peaks), '. touch of force plate');
+                            time(sync_peaks), '. touch of force plate');
 
 % Store all accelerations in time series collection.
 a_tsc = tscollection({a_trunk, a_thigh, a_shank});
 
+close all;
 hold off;
-figure();
-plot(a_trunk);
+plot(a_shank);
+
+%%
 
 % -------------------------------------------------------------------------
 % 5) Synchronise and store seperate force plate cycles in a time series
@@ -154,7 +219,7 @@ plot(a_trunk);
 % -------------------------------------------------------------------------
 
 % Compute number of selected cycles.
-n_cycles = length(peaks);
+n_cycles = length(sync_peaks);
 
 % Create empty time series object and name it.
 data_FP_concat_rs = timeseries();
@@ -167,7 +232,7 @@ time_FP = (FP_data_complete{i, 1}(FP_data_complete{i, 2}: ...
            FP_data_complete{i, 3}, 1)') / 1000;
 
 % Calculate bias and correct time axis of force plate data.
-time_bias = time(peaks(i)) - time_FP(1);
+time_bias = time(sync_peaks(i)) - time_FP(1);
 
 time_FP_corr = time_FP + time_bias;
 
@@ -180,7 +245,7 @@ data_FP.DataInfo.Units = 'N';
 
 % Add event (point in time when patient touches the force plate)
 event = tsdata.event(strcat(num2str(i), '. touch of force plate'), ...
-                     time(peaks(i)));
+                     time(sync_peaks(i)));
 event.Units = 'seconds';
 data_FP = addevent(data_FP, event);
 
